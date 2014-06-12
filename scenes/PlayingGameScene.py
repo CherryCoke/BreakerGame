3fd9498659a78930a3d90add31c09fd43fc6d035
CherryCoke <tbonnette25@gmail.com>
2014-06-12 01:05:21 -0700

    Added collision detection for level one

--- scenes/PlayingGameScene.py
+++ scenes/PlayingGameScene.py
@@ -9,16 +9,22 @@ class PlayingGameScene(Scene):
 
 	def render(self):
 		super(PlayingGameScene, self).render()
-
 		game = self.getGame()
 
 		for ball in game.getBalls():
+			for brick in game.getLevel().getBricks():
+				if ball.intersects(brick):
+					brick.hit()
+					ball.changeDirection(brick)
+					break
 			ball.updatePosition()
-
 			game.screen.blit(ball.getSprite(), ball.getPosition())
-
-		for bricks in game.getLevel().getBricks():
-			game.screen.blit(bricks.getSprite(), bricks.getPosition())
+		
+		for brick in game.getLevel().getBricks():
+   			if not brick.isDestroyed():
+   				game.screen.blit(brick.getSprite(), brick.getPosition())
+        
+        
 
 	def handleEvents(self, events):
 		super(PlayingGameScene, self).handleEvents(events)
